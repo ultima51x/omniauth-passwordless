c568928a08f49ec6ae949456569c577745b55375
David Hwang <d.hw4ng@gmail.com>
2014-07-24 17:41:10 -0700

    fleshing out passwordless strategy

--- lib/omniauth/strategies/passwordless.rb
+++ lib/omniauth/strategies/passwordless.rb
@@ -5,17 +5,32 @@ module OmniAuth
     class Passwordless
       include OmniAuth::Strategy
 
-      option :fields, [:email]
-
       def request_phase
         form = OmniAuth::Form.new(:title => "User Info", :url => callback_path)
-        options.fields.each do |field|
-          form.text_field field.to_s.capitalize.gsub("_", " "), field.to_s
-        end
+        form.text_field "Email", "email"
         form.button "Enter"
         form.to_response
       end
 
+      def callback_phase
+        return fail!(:invalid_email) unless valid_email
+        return fail!(:invalid_credentials) unless authenticate
+      end
+
+      def valid_email
+        # Just checking for a @ character in the string
+        /@/.match(request.params["email"])
+      end
+
+      def model
+        options[:model] || ::PasswordlessAccount
+      end
+
+      def authenticate
+        email = request.params["email"]
+        object = model.where(:email => email.downcase).first_or_create
+        object.valid?
+      end
     end
   end
 end
